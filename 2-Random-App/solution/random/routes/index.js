9e8df605f6123abb8a3c809cb22ab7e141bea610
m0ne <michael.schneider@hispeed.ch>
2020-10-16 17:31:28 +0200

    removed unused data

--- 2-Random-App/solution/random/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const randomController = require('../controllers/randomController');
-
-router.get('/',  randomController.index);
-router.get('/random', randomController.renderRandomResult);
-router.post('/random', randomController.redirectToRandomResult);
-
-module.exports = router;
--- 2-Random-App/solution/random/services/randomService.js
+++
@@ -1,9 +0,0 @@
-function getRandomList(count, min, max) {
-    return [...Array(count).keys()].map(i => getRandom(min, max));
-}
-
-function getRandom(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-}
-
-module.exports = {getRandom, getRandomList};
